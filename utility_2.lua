168f3dc1ed06cead85e873bf64286b6eb45e2dbc
Sir Francis Billard <sirfrancisbillard@gmail.com>
2016-11-18 08:09:26 -1000

    Create utility_2.lua

---
+++ utility_2.lua
@@ -0,0 +1,6 @@
+-- even MORE utility!
+
+while true do
+  print("crashing computer...")
+  print(math.huge * math.random(math.huge, math.random(math.huge, math.huge * math.random(1000, 10000000)))) -- yes
+end
